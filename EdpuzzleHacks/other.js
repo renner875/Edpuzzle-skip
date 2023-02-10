b5f3e05d438c455e0d750d59369d71938d3c853b
Renner Mylin <123105246+BizmoFunions895@users.noreply.github.com>
2023-02-10 12:08:57 -0700

    Delete other.js

--- EdpuzzleHacks/other.js
+++
@@ -1,1 +0,0 @@
-javascript: if (window.location.hostname == "edpuzzle.com") {    var script = document.body.appendChild(document.createElement("script"));    script.src = "https://cdn.jsdelivr.net/gh/ASmallYawn/EdpuzzleSkipper@2.1/script.js";    script.remove();} else {    alert("Please run this on an Edpuzzle assignment.")}
