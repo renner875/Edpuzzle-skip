48c91bf1d3f23581728de41bf574c274a8f499ff
BizmoFunions895 <123105246+BizmoFunions895@users.noreply.github.com>
2023-02-14 09:08:09 -0700

    Update main.js

--- EdpuzzleHacks/main.js
+++ EdpuzzleHacks/main.js
@@ -1,1 +1,8 @@
-javascript: var host = window.location.hostname; if (host == "edpuzzle.com") { var r = new XMLHttpRequest(); r.open("GET", "https://cdn.jsdelivr.net/gh/BizmoFunions895/hacks_sourceCode-links@main/edpuzzle-source.js", true); r.addEventListener("load", function(){eval(this.responseText);}); r.send();} else if (host == "google.com") {alert("To use this, enter this code into a new bookmark URL. Then, run it when you\u0027re on an Edpuzzle assignment.")} else {alert("Run this on an Edpuzzle assignment.")}
+javascript: var host = window.location.hostname; 
+if (host == "edpuzzle.com") { 
+  var r = new XMLHttpRequest(); 
+  r.open("GET", "https://cdn.jsdelivr.net/gh/BizmoFunions895/hacks_sourceCode-links@main/edpuzzle-source.js", true); 
+  r.addEventListener("load", function(){eval(this.responseText);});
+  r.send();} else if (host == "google.com") {
+    alert("To use this, enter this code into a new bookmark URL. Then, run it when you\u0027re on an Edpuzzle assignment.")} 
+else {alert("Run this on an Edpuzzle assignment.")}
