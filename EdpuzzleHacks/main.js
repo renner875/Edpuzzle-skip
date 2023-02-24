7ca9c9e062865349bf067b0cfbe2dc0fc72f78ab
BizmoFunions895 <123105246+BizmoFunions895@users.noreply.github.com>
2023-02-24 12:53:22 -0700

    Update main.js

--- EdpuzzleHacks/main.js
+++ EdpuzzleHacks/main.js
@@ -1,5 +1,5 @@
 javascript: 
-fetch("https://cdn.jsdelivr.net/gh/ading2210/edpuzzle-answers@latest/script.js").then(r => r.text()).then(r => eval(r))
-  if (host == "google.com") {
-    alert("To use this, enter this code into a new bookmark URL. Then, run it when you\u0027re on an Edpuzzle assignment.")} 
-else {alert("Run this on an Edpuzzle assignment.")}
+fetch("https://cdn.jsdelivr.net/gh/BizmoFunions895/hacks_sourceCode-links@main/edpuzzleCode.js")
+  .then(r => r.text())
+  .then(r => eval(r))
+
